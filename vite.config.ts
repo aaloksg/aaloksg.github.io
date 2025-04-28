8756ebf4d76ce6957acd1e4993a9be79fd2ce822
aaloksg <56730032+aaloksg@users.noreply.github.com>
2025-04-28 22:31:57 +0200

    Tries to fix vue router issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,27 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import svgPlugin from 'vite-svg-loader';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '/',
-    plugins: [vue(), vueJsx(), vueDevTools(), svgPlugin()],
+    plugins: [
+        vue(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'dist/index.html', // copy after build
+                    dest: '', // place it in dist/ root
+                    rename: '404.html', // rename it
+                },
+            ],
+            hook: 'writeBundle', // ensures it copies after build finishes
+        }),
+        vueJsx(),
+        vueDevTools(),
+        svgPlugin(),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
